45cb1538358f861774e4395186f81c52e78a96f9
theseda2s <theseda2s@naver.com>
2022-10-11 20:33:19 +0900

    💄 Create and apply overlay layout

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,16 @@
 const openMenuButton = document.querySelector('.util-left .menu-button')
 const sidebar = document.querySelector('.sidebar')
+const overlay = document.querySelector('.overlay')
 const closeButton = sidebar.querySelector('.close-button')
 
 function openSidebar() {
   sidebar.classList.add('is-active')
+  overlay.classList.add('is-active')
 }
 openMenuButton.addEventListener('click', openSidebar)
 
 function closeSidebar() {
   sidebar.classList.remove('is-active')
+  overlay.classList.remove('is-active')
 }
 closeButton.addEventListener('click', closeSidebar)

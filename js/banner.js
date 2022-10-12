6ce86e3d6a2feda319cb0cdac0f1ef8700d44330
theseda2s <theseda2s@naver.com>
2022-10-12 21:05:37 +0900

    💄 Implement main-banner slider

---
+++ js/banner.js
@@ -0,0 +1,23 @@
+const mobileSwiper = new Swiper('.main-banner.desktop-hidden .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.main-banner.desktop-hidden .swiper-pagination',
+    clickable: true,
+  },
+})
+
+const desktopSwiper = new Swiper('.main-banner.desktop-only .swiper', {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.main-banner.desktop-only .swiper-pagination',
+    clickable: true,
+  },
+})

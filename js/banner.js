49a90638557d97c4a3403594c3298e07a93ac9a7
theseda2s <theseda2s@naver.com>
2023-03-13 18:44:19 +0900

    💄 Implement swiper-autoplay button

--- js/banner.js
+++ js/banner.js
@@ -3,7 +3,7 @@
 const mobileSwiper = new Swiper('.main-banner.desktop-hidden .swiper', {
   loop: true,
   autoplay: {
-    delay: 2500,
+    delay: 2000,
     disableOnInteraction: false,
   },
   pagination: {
@@ -15,11 +15,28 @@ const mobileSwiper = new Swiper('.main-banner.desktop-hidden .swiper', {
 const desktopSwiper = new Swiper('.main-banner.desktop-only .swiper', {
   loop: true,
   autoplay: {
-    delay: 2500,
+    delay: 2000,
     disableOnInteraction: false,
   },
   pagination: {
     el: '.main-banner.desktop-only .swiper-pagination',
     clickable: true,
+  },
+  on: {
+    init: function () {
+      const autoPlayBtn = document.querySelector(
+        '.main-banner.desktop-only .swiper-autoplay-control > button'
+      )
+      autoPlayBtn.addEventListener('click', (e) => {
+        let autoPlayState = autoPlayBtn.getAttribute('aria-pressed')
+        if (autoPlayState === 'false') {
+          autoPlayBtn.setAttribute('aria-pressed', 'true')
+          this.autoplay.stop()
+        } else if (autoPlayState === 'true') {
+          autoPlayBtn.setAttribute('aria-pressed', 'false')
+          this.autoplay.start()
+        }
+      })
+    },
   },
 })
